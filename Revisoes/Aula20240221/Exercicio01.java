8ecb7a5469c9dc971386c9e50012786ac02c4db6
wagnerfloresdossantos <wagner.fs@aluno.ifsc.edu.br>
2024-02-22 16:02:14 -0300

    inserindo comentários

--- Revisoes/Aula20240221/Exercicio01.java
+++ Revisoes/Aula20240221/Exercicio01.java
@@ -4,26 +4,31 @@ public class Exercicio01 {
 
     public static void main(String[] args){
 
+        // Criado objeto teclado para ler as informações
         Scanner teclado = new Scanner(System.in);
-        
+
+        // Imprimindo na tela
         System.out.print("Entre com o nome: ");
-        
+
+        // Lendo a informação do teclado para a Stirng "nome"
         String nome = teclado.nextLine();
 
+        // Impriimindo na tela
         System.out.print("Entre com o ano que nasceu: ");
-        
+
+        // Lendo a informação do teclado para o int "anoNasc"
         int anoNasc = teclado.nextInt();
-        
+
+        // Impriimindo na tela
         System.out.print("Entre com o ano Atual: ");
-        
+
+        // Lendo a informação do teclado para o int "anoAtual"
         int anoAtual = teclado.nextInt();
-        
+
+        // Calculado a idade
         int idade = (anoAtual - anoNasc);
-
-        System.out.println(nome + ", possui " + idade + " anos") ;
         
-
-
+        // Imprimindo na tela
+        System.out.println(nome + ", possui " + idade + " anos") ;
     }
-
 }
