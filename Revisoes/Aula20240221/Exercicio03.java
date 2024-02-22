7ffe5523f586572153f061dfcedbf4aa7606122a
wagnerfloresdossantos <wagner.fs@aluno.ifsc.edu.br>
2024-02-22 19:09:19 -0300

    inserido comentários

--- Revisoes/Aula20240221/Exercicio03.java
+++ Revisoes/Aula20240221/Exercicio03.java
@@ -5,20 +5,30 @@ public class Exercicio03 {
 
     public static void main(String[] args) {
     
+    // Cria objeto teclado     
     Scanner teclado = new Scanner(System.in);    
 
+    // obtendo numeros pseudo aletários de 1 a 100 
     Random r = new Random();
 
+    // atribui o numero sorteado a variável
     int numeroSorteado = r.nextInt(100)+1;
 
+    // variável do palpite fornecido para o usuário
     int palpite;
+
+    // variavel para o contador de tentativas
     int tentativas = 0;
+    
+    // variavel para verificação de palpite duplicado em sequência
     int palpiteAnterior = 0;
     
-    System.out.println("tente descobrir um numero entre 1 e 100");
 
+    System.out.println("tente descobrir um numero entre 1 e 100");
+        
+        // loop para entativas enquanto o numro sorteado não é igual ao do palpite
         do {
-         System.out.println("Digite o Papite");
+         System.out.print("Digite o Papite: ");
 
          palpite = teclado.nextInt();
          tentativas++;
@@ -27,7 +37,7 @@ public class Exercicio03 {
             System.out.println("Você escolheu 2 vezes o número: " + palpite);
         } else{
              if (palpite == numeroSorteado){
-                 System.out.println("Parabéns você venceu");
+                 System.out.println("Parabéns você acertou em " + tentativas + ".");
              } else if (palpite < numeroSorteado) {
                 System.out.println("Experimente um número maior, o número sorteado está entre " + (palpite + 1) + " e 100");
              } else {
